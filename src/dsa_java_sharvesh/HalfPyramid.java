c893847d9e3324fad9faf2893c04936b0f968ab2
sharvesh <sharveshsreethar0806@gmail.com>
2023-03-30 19:36:29 +0530

    HolloRectangle Decreasing Triangle done

---
+++ src/dsa_java_sharvesh/HalfPyramid.java
@@ -0,0 +1,12 @@
+package dsa_java_sharvesh;
+
+public class HalfPyramid {
+    public static void main(String arge[]){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
