ef16a48d241a1a5638db73985e35a54964adb672
sharvesh <sharveshsreethar0806@gmail.com>
2023-04-02 19:08:27 +0530

    Number pattern started

--- src/dsa_java_sharvesh/NumberPattern1.java
+++ src/dsa_java_sharvesh/NumberPattern1.java
@@ -1,10 +1,18 @@
 package dsa_java_sharvesh;
-//Decreasing Triangle
-public class HalfPyramid2 {
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+ */
+public class NumberPattern1
+{
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print("* ");
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
--- src/dsa_java_sharvesh/NumberPattern2.java
+++ src/dsa_java_sharvesh/NumberPattern2.java
@@ -1,10 +1,10 @@
 package dsa_java_sharvesh;
-//Decreasing Triangle
-public class HalfPyramid2 {
+
+public class NumberPattern2 {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print("* ");
+            for(int j=1;j<=(5-i+1);j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
