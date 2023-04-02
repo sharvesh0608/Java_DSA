59f50cd93d36a2f9566bde744cdc93f02b2dd199
sharvesh <sharveshsreethar0806@gmail.com>
2023-04-02 19:21:02 +0530

    Floyd's Triangle implemtend

---
+++ src/dsa_java_sharvesh/FloydTriangle.java
@@ -0,0 +1,14 @@
+package dsa_java_sharvesh;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int counter=0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                counter++;
+                System.out.print(counter+" ");
+            }
+            System.out.println();
+        }
+    }
+}
