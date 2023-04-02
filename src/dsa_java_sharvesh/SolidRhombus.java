0e022e1402b5a3d802a6d4a4e877bdae52f41492
sharvesh <sharveshsreethar0806@gmail.com>
2023-04-02 20:34:36 +0530

    Rhombus Pattern Implemented

---
+++ src/dsa_java_sharvesh/SolidRhombus.java
@@ -0,0 +1,18 @@
+package dsa_java_sharvesh;
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++ ){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
