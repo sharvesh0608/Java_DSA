0b40d6dcb032df126234279d6d90c60f29e0c33c
sharvesh <sharveshsreethar0806@gmail.com>
2023-04-03 19:46:51 +0530

    Palindrome Pattern implemented

---
+++ src/dsa_java_sharvesh/PalindromePattern.java
@@ -0,0 +1,18 @@
+package dsa_java_sharvesh;
+
+public class PalindromePattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
