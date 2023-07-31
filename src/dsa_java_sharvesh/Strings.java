97770993bf4440f3e98821580ee306d01fd4fcab
sharvesh <sharveshsreethar0806@gmail.com>
2023-07-31 19:42:19 +0530

    Strings basics

---
+++ src/dsa_java_sharvesh/Strings.java
@@ -0,0 +1,36 @@
+package dsa_java_sharvesh;
+import java.util.Scanner;
+public class Strings {
+
+    public static void main(String[] args) {
+//        String str ="Happy";
+//        String str1="Birthday";
+//        String finalstr =str+" "+str1;
+//        System.out.println(finalstr);
+
+//        Scanner sc = new Scanner(System.in);
+//        String newStrLine=sc.nextLine();
+        //System.out.println(newStrLine.length()+" "+newStrLine);
+
+        //Access Each character of a String
+//        for(int i=0;i<newStrLine.length();i++){
+//            System.out.println(newStrLine.charAt(i));
+//        }
+
+        //Compare 2 strings
+//        String str1= "Happy",str2="Aappy";
+//        if(str1.compareTo(str2)==0){
+//            System.out.println("They are equal");
+//        }
+//        else if(str1.compareTo(str2)>0){
+//            System.out.println(" str1 > str2");
+//        }
+//        else if(str1.compareTo(str2)<0){
+//            System.out.println("str1 < str2");
+//        }
+
+//        String Str="12345";
+//        int number= Integer.parseInt(Str);
+//        System.out.println(number);
+    }
+}
